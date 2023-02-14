98459b43b1fc6d2f7c51594f26c1faa83358338e
duccanhole <duccanhole@gmail.com>
2023-02-14 23:12:24 +0700

    init repo

---
+++ src/components/Card.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="border-2 rounded-lg p-1"
+    :style="{ width: '250px', height: '300px' }"
+  >
+    <div class="bg-pink-200 rounded-lg h-full relative">
+      <div class="absolute left-0 top-0 bg-white rounded-br-md p-1 px-2 font-caveat font-bold">intro</div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="underline font-bold">
+    This is Test page {{ count }}
+    <button @click="count++">click:</button>
+    <card />
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+import Card from "../components/Card.vue";
+
+const count = ref(0);
+</script>
